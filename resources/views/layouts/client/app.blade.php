cddb05cb8887800edb3f5a3b2c892f5b65d0da8c
ntq2k3work <ntq2k3.work@gmail.com>
2024-10-29 23:10:23 +0700

    devide into layouts

---
+++ resources/views/layouts/client/app.blade.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="vi">
+   <head>
+      <meta charset="utf-8" />
+      <meta content="width=device-width,initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+      <meta name="theme-color" content="#234556">
+      <meta http-equiv="Content-Language" content="vi" />
+      <meta content="VN" name="geo.region" />
+      <meta name="DC.language" scheme="utf-8" content="vi" />
+      <meta name="language" content="Việt Nam">
+
+
+      <link rel="shortcut icon" href="https://www.pngkey.com/png/detail/360-3601772_your-logo-here-your-company-logo-here-png.png" type="image/x-icon" />
+      <meta name="revisit-after" content="1 days" />
+      <meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+      <title>Phim hay 2021 - Xem phim hay nhất</title>
+      <meta name="description" content="Phim hay 2021 - Xem phim hay nhất, xem phim online miễn phí, phim hot , phim nhanh" />
+      <link rel="canonical" href="">
+      <link rel="next" href="" />
+      <meta property="og:locale" content="vi_VN" />
+      <meta property="og:title" content="Phim hay 2020 - Xem phim hay nhất" />
+      <meta property="og:description" content="Phim hay 2020 - Xem phim hay nhất, phim hay trung quốc, hàn quốc, việt nam, mỹ, hong kong , chiếu rạp" />
+      <meta property="og:url" content="" />
+      <meta property="og:site_name" content="Phim hay 2021- Xem phim hay nhất" />
+      <meta property="og:image" content="" />
+      <meta property="og:image:width" content="300" />
+      <meta property="og:image:height" content="55" />
+      <link rel='dns-prefetch' href='//s.w.org' />
+      <link rel='stylesheet' id='bootstrap-css' href="{{ asset('assets/css/bootstrap.min.css?ver=5.7.2') }}" media='all' />
+      <link rel='stylesheet' id='style-css' href="{{ asset('assets/css/style.css?ver=5.7.2') }}" media='all' />
+      <link rel='stylesheet' id='wp-block-library-css' href="{{ asset('assets/css/style.min.css?ver=5.7.2') }}" media='all' />
+      <script type='text/javascript' src="{{ asset('assets/js/jquery.min.js?ver=5.7.2') }}" id='halim-jquery-js'></script>
+      <style type="text/css" id="wp-custom-css">
+         .textwidget p a img {
+         width: 100%;
+         }
+      </style>
+      <style>#header .site-title {background: url(https://www.pngkey.com/png/detail/360-3601772_your-logo-here-your-company-logo-here-png.png) no-repeat top left;background-size: contain;text-indent: -9999px;}</style>
+   </head>
+   <body class="home blog halimthemes halimmovies" data-masonry="">
+    {{-- Header --}}
+        @include('layouts.client.header')
+      <div class="navbar-container">
+         <div class="container">
+            @include('layouts.client.menu')
+            <div class="collapse navbar-collapse" id="search-form">
+               <div id="mobile-search-form" class="halim-search-form"></div>
+            </div>
+            <div class="collapse navbar-collapse" id="user-info">
+               <div id="mobile-user-login"></div>
+            </div>
+         </div>
+      </div>
+      </div>
+
+      <div class="container">
+         <div class="row fullwith-slider"></div>
+      </div>
+      {{-- Content --}}
+      @yield('content')
+      <div class="clearfix"></div>
+      @include('layouts.client.footer')
+      <div id='easy-top'></div>
+
+      <script type='text/javascript' src='js/bootstrap.min.js?ver=5.7.2' id='bootstrap-js'></script>
+      <script type='text/javascript' src='js/owl.carousel.min.js?ver=5.7.2' id='carousel-js'></script>
+
+      <script type='text/javascript' src='js/halimtheme-core.min.js?ver=1626273138' id='halim-init-js'></script>
+
+
+
+
+      <style>#overlay_mb{position:fixed;display:none;width:100%;height:100%;top:0;left:0;right:0;bottom:0;background-color:rgba(0, 0, 0, 0.7);z-index:99999;cursor:pointer}#overlay_mb .overlay_mb_content{position:relative;height:100%}.overlay_mb_block{display:inline-block;position:relative}#overlay_mb .overlay_mb_content .overlay_mb_wrapper{width:600px;height:auto;position:relative;left:50%;top:50%;transform:translate(-50%, -50%);text-align:center}#overlay_mb .overlay_mb_content .cls_ov{color:#fff;text-align:center;cursor:pointer;position:absolute;top:5px;right:5px;z-index:999999;font-size:14px;padding:4px 10px;border:1px solid #aeaeae;background-color:rgba(0, 0, 0, 0.7)}#overlay_mb img{position:relative;z-index:999}@media only screen and (max-width: 768px){#overlay_mb .overlay_mb_content .overlay_mb_wrapper{width:400px;top:3%;transform:translate(-50%, 3%)}}@media only screen and (max-width: 400px){#overlay_mb .overlay_mb_content .overlay_mb_wrapper{width:310px;top:3%;transform:translate(-50%, 3%)}}</style>
+
+      <style>
+         #overlay_pc {
+         position: fixed;
+         display: none;
+         width: 100%;
+         height: 100%;
+         top: 0;
+         left: 0;
+         right: 0;
+         bottom: 0;
+         background-color: rgba(0, 0, 0, 0.7);
+         z-index: 99999;
+         cursor: pointer;
+         }
+         #overlay_pc .overlay_pc_content {
+         position: relative;
+         height: 100%;
+         }
+         .overlay_pc_block {
+         display: inline-block;
+         position: relative;
+         }
+         #overlay_pc .overlay_pc_content .overlay_pc_wrapper {
+         width: 600px;
+         height: auto;
+         position: relative;
+         left: 50%;
+         top: 50%;
+         transform: translate(-50%, -50%);
+         text-align: center;
+         }
+         #overlay_pc .overlay_pc_content .cls_ov {
+         color: #fff;
+         text-align: center;
+         cursor: pointer;
+         position: absolute;
+         top: 5px;
+         right: 5px;
+         z-index: 999999;
+         font-size: 14px;
+         padding: 4px 10px;
+         border: 1px solid #aeaeae;
+         background-color: rgba(0, 0, 0, 0.7);
+         }
+         #overlay_pc img {
+         position: relative;
+         z-index: 999;
+         }
+         @media only screen and (max-width: 768px) {
+         #overlay_pc .overlay_pc_content .overlay_pc_wrapper {
+         width: 400px;
+         top: 3%;
+         transform: translate(-50%, 3%);
+         }
+         }
+         @media only screen and (max-width: 400px) {
+         #overlay_pc .overlay_pc_content .overlay_pc_wrapper {
+         width: 310px;
+         top: 3%;
+         transform: translate(-50%, 3%);
+         }
+         }
+      </style>
+
+      <style>
+         .float-ck { position: fixed; bottom: 0px; z-index: 9}
+         * html .float-ck /* IE6 position fixed Bottom */{position:absolute;bottom:auto;top:expression(eval (document.documentElement.scrollTop+document.docum entElement.clientHeight-this.offsetHeight-(parseInt(this.currentStyle.marginTop,10)||0)-(parseInt(this.currentStyle.marginBottom,10)||0))) ;}
+         #hide_float_left a {background: #0098D2;padding: 5px 15px 5px 15px;color: #FFF;font-weight: 700;float: left;}
+         #hide_float_left_m a {background: #0098D2;padding: 5px 15px 5px 15px;color: #FFF;font-weight: 700;}
+         span.bannermobi2 img {height: 70px;width: 300px;}
+         #hide_float_right a { background: #01AEF0; padding: 5px 5px 1px 5px; color: #FFF;float: left;}
+      </style>
+   </body>
+</html>
