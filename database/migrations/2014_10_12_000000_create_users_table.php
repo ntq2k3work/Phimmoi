50139b6d8027ec9e801a7be9de824b9ffbe524de
ntq2k3work <ntq2k3.work@gmail.com>
2024-10-29 22:26:44 +0700

    base migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('name');
+            $table->date('birthday')->nullable();
+            $table->integer('gender');
+            $table->string('phone_number')->unique();
+            $table->string('address')->nullable();
+            $table->string('avatar_url')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
