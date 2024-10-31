493281bf029fd10fc0de3d0606ac952ea05919be
ntq2k3work <ntq2k3.work@gmail.com>
2024-10-31 13:56:20 +0700

    recheck test

--- tests/Feature/Users/GetListUserTest.php
+++ tests/Feature/Users/GetListUserTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Users;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class GetListUserTest extends TestCase
@@ -13,7 +14,7 @@ class GetListUserTest extends TestCase
     public function authenticate_user_can_get_list_user(): void
     {
         $response = $this-> get(route('admin.users'));
-        $response->assertStatus(200);
+        $response->assertRedirect(route('login'));
     }
 
     /** @test */
@@ -21,6 +22,6 @@ class GetListUserTest extends TestCase
     {
         $this->actingAs(User::factory()->create());
         $response = $this-> get(route('admin.users'));
-        $response->assertStatus(200);
+        $response->assertStatus(Response::HTTP_OK);
     }
 }
